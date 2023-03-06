6b84e3d50394ce901e105caf90cec35f3899f174
Abhishek2020xyz <116792007+Abhishek2020xyz@users.noreply.github.com>
2023-03-06 10:04:17 +0530

    Add files via upload

--- src/components/store/Index.js
+++ src/components/store/Index.js
@@ -3,12 +3,17 @@ import {createStore} from 'redux';
 const counterReducer = (state = {counter: 0}, action) => {
     if (action.type === 'increment'){
         return{
-            counter: state.counter + 2,
+            counter: state.counter + 1,
+        };
+    }
+    if (action.type === 'increase'){
+        return{
+            counter: state.counter + action.amount,
         };
     }
     if (action.type === 'decrement') {
         return{
-            counter: state.counter - 2,
+            counter: state.counter - 1,
         }
     }
     return state;
